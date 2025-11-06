d5c91d420b9cde0ffa526935b3faf2188550a213
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2025-11-06 23:42:58 +0530

    updated complete profile

--- server/src/routes/student.routes.js
+++ server/src/routes/student.routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { authenticate, isStudent, isStudentVerified } = require("../middlewares/auth.middleware");
+const { authenticate, isStudent, isStudentVerified, isProfileCompleted } = require("../middlewares/auth.middleware");
 const { getProfile, completeProfile } = require("../controllers/profile.controller");
 
-router.get("/getprofile", authenticate, isStudent, isStudentVerified, getProfile);
-router.put("/updateprofile", authenticate, isStudent, isStudentVerified, completeProfile);
+router.get("/getprofile", authenticate, isStudent, isStudentVerified, isProfileCompleted, getProfile);
+router.put("/completeprofile", authenticate, isStudent, isStudentVerified, completeProfile);
 
 module.exports = router;
