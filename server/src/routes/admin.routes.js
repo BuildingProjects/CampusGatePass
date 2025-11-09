4f9aeec26930efd3bfab0997f3ed28577302e5a2
ashutosh-3474 <ashutosh.2201062cs@iiitbh.ac.in>
2025-11-09 22:55:30 +0530

    admin middleware

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { registerEmployee } = require("../controllers/registerEmployee.controller");
+const { authenticate, isAdmin } = require("../middlewares/auth.middleware");
 const router = express.Router();
 // Register a new guard
 
-router.post("/registeremployee", registerEmployee);
+router.post("/registeremployee",authenticate, isAdmin, registerEmployee);
 
 module.exports = router;
