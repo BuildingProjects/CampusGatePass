fb2efe84d3ded10db18259aec433dcd03f183cfb
Roshu2003 <roshan.2201021cs@iiitbh.ac.in>
2025-11-10 10:39:54 +0530

    styling fix

--- client/App.tsx
+++ client/App.tsx
@@ -22,61 +22,62 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="RoleSelector"
+        initialRouteName='RoleSelector'
         screenOptions={{
           headerTitleAlign: "center",
           headerStyle: { backgroundColor: "#0F172A" },
           headerTintColor: "#fff",
+          headerShown: false,
         }}
       >
         <Stack.Screen
-          name="RoleSelector"
+          name='RoleSelector'
           component={RoleSelector}
-          options={{ headerShown: false }}
+          options={{ headerShown: false, gestureEnabled: false }}
         />
 
         <Stack.Screen
-          name="SignupScreen"
+          name='SignupScreen'
           component={SignupScreen}
           options={{ headerTitle: "" }} // ✅ keep header but hide title
         />
         <Stack.Screen
-          name="AdminHome"
+          name='AdminHome'
           component={AdminHome}
           options={{ headerShown: false }} // ✅ keep header but hide title
         />
 
         <Stack.Screen
-          name="LoginScreen"
+          name='LoginScreen'
           component={LoginScreen}
           options={{ headerTitle: "" }} // ✅ same here
         />
 
         {/* ✅ Add Student Home (with bottom tab navigation) */}
         <Stack.Screen
-          name="StudentHome"
+          name='StudentHome'
           component={StudentHome}
           options={{ headerShown: false, gestureEnabled: false }}
         />
         <Stack.Screen
-          name="GuardHome"
+          name='GuardHome'
           component={GuardHome}
           options={{ headerShown: false }}
         />
 
         {/* ✅ Add Student Profile (in case you want direct access) */}
         <Stack.Screen
-          name="StudentProfile"
+          name='StudentProfile'
           component={StudentProfile}
           options={{ headerTitle: "Edit Profile" }}
         />
         <Stack.Screen
-          name="OTPScreen"
+          name='OTPScreen'
           component={OTPScreen}
           options={{ headerTitle: "" }}
         />
         <Stack.Screen
-          name="ProfileCompletionScreen"
+          name='ProfileCompletionScreen'
           component={ProfileCompletionScreen}
           options={{ headerTitle: "" }}
         />
@@ -92,7 +93,7 @@ export default function App() {
         />
 
         <Stack.Screen
-          name="ViewLogsScreen"
+          name='ViewLogsScreen'
           component={ViewLogsScreen}
           options={{ headerShown: false }}
         />
