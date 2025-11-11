9af4ceddaf90573456460109efa3bd0d1bdba27a
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2025-11-11 21:47:26 +0530

    hosting backend

--- server/src/server.js
+++ server/src/server.js
@@ -9,7 +9,13 @@ const studentRoutes = require("./routes/student.routes");
 const adminRoutes = require("./routes/admin.routes");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // ✅ allow all for mobile app
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 connectDB();
